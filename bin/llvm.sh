cb1d9b8a7616f368eb6c3e685da822d2a67ad2de
Noah Zoschke <noah@heroku.com>
2011-09-19 11:52:40 -0500

    fix script portability, $TMP => /tmp

--- bin/llvm.sh
+++ bin/llvm.sh
@@ -1,19 +1,20 @@
-#!/usr/bin/env bash -ex
+#!/usr/bin/env bash
+set -ex
+
 # http://llvm.org/docs/GettingStarted.html
 
-TMP=/tmp/exercise
-mkdir $TMP && cd $TMP
+cd /tmp
 
 git clone http://llvm.org/git/llvm.git
 git clone http://llvm.org/git/clang.git llvm/tools/clang
 
 envs[1]="CC=gcc CXX=g++"
-envs[2]="PATH=$TMP/stage1/Release/bin:$PATH CC=clang CXX=clang++"
-envs[3]="PATH=$TMP/stage2/Release/bin:$PATH CC=clang CXX=clang++"
+envs[2]="PATH=/tmp/stage1/Release/bin:$PATH CC=clang CXX=clang++"
+envs[3]="PATH=/tmp/stage2/Release/bin:$PATH CC=clang CXX=clang++"
 
 for i in ${!envs[*]}; do
   (
-    mkdir -p $TMP/stage$i && cd $TMP/stage$i
+    mkdir -p /tmp/stage$i && cd /tmp/stage$i
     export ${envs[$i]}
     which $CC $CXX
     ../llvm/configure --enable-optimized --disable-assertions --enable-targets=host-only
@@ -23,8 +24,8 @@ for i in ${!envs[*]}; do
 done
 
 # Diff binaries
-diff <(strings $TMP/stage1/Release/bin/clang) <(strings $TMP/stage2/Release/bin/clang) | wc
+diff <(strings /tmp/stage1/Release/bin/clang) <(strings /tmp/stage2/Release/bin/clang) | wc
 # 422166  939811 4695061
 
-diff <(strings $TMP/stage2/Release/bin/clang) <(strings $TMP/stage3/Release/bin/clang) | wc
+diff <(strings /tmp/stage2/Release/bin/clang) <(strings /tmp/stage3/Release/bin/clang) | wc
 #   3246    5233   27606
